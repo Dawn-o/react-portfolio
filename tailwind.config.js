058479350f9a75705d4b146019b772ec2137be0a
Rushel <ruselaq@gmail.com>
2024-12-18 21:19:27 +0800

    feat: Install tailwindcss, first setup and clean up unused files

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      primary: "Montserrat",
+      secondary: "Orbitron",
+    },
+    extend: {
+      colors: {
+        primary: "#B809C3",
+        secondary: "#ff014f",
+        third: "#FEF6C7",
+        fourth: "#011eff",
+      },
+    },
+  },
+  plugins: [],
+}
+
